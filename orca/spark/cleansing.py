27e74ca9fb00352fb10081be9e225c950c118b5c
Krishna <gopalkrishna.naredla@gmail.com>
2020-11-04 17:40:14 -0400

    modified dedups

--- orca/spark/cleansing.py
+++ orca/spark/cleansing.py
@@ -10,7 +10,7 @@ def dedupsByRank(
     df: DataFrame,
     partitionCols: List,
     orderCols: Union[List, Dict[str, List]],
-    rankType: Column = row_number(),
+    rankType:Column = row_number(),
 ) -> DataFrame:
     orderCols = (
         {"columns": list(orderCols), "asc": [], "desc": []}
@@ -23,10 +23,10 @@ def dedupsByRank(
         else (col(col_name).desc() if col_name in orderCols["desc"] else col(col_name))
     )
     windowBy = Window.partitionBy(
-        *list(map(lambda col_name: col(col_name), partitionBy))
-    ).orderBy(*list(map(lambda col_name: _(col_name), orderCols)))
+        *list(map(lambda col_name: col(col_name), partitionCols))
+    ).orderBy(*list(map(lambda col_name: _(col_name), orderCols['columns'])))
     return (
-        df.withColumn("rank", rankType.over(windowPartition))
+        df.withColumn("rank", rankType.over(windowBy))
         .filter(col("rank") == 1)
         .drop("rank")
     )
